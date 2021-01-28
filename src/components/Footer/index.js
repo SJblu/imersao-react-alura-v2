0e44d3c47bbe64c604214f87767c6b8495ed87e1
SILVIO SOUZA <sjblu7@gmail.com>
2021-01-28 19:54:36 -0400

    Feito personalizacoes iniciais

---
+++ src/components/Footer/index.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+// src/components/Footer/index.js
+const FooterWrapper = styled.footer`
+  background-color: #372e7995;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  border-radius: 4px; 
+  img {
+    width: 58px;
+    margin-right: 23px;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+    transition: .3s;
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+    span {
+      text-decoration: underline;
+    }
+  }
+  p {
+    font-weight: bold;
+    text-shadow: 1px 1px 2px #FFFFFF;
+  }
+
+  span {
+    text-shadow: 0px 0px 0px;
+    font-weight: normal;
+  }
+`;
+
+export default function Footer(props) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FooterWrapper {...props}>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+      <p class="texto-footer">
+        Orgulhosamente criado durante
+        {' '}
+        a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          <span class="link-imersao">Imersão React da Alura</span>
+        </a>
+      </p>
+    </FooterWrapper>
+  );
+}
